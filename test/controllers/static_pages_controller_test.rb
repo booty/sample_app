101106c310d21e62757a88be8c1d82c118f2e3cd
booty <johnedmundrose@gmail.com>
2019-01-08 19:10:56 -0500

    Add bootstrap, tests, partials

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,18 +2,24 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", "Help | Sample App"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | Sample App"
   end
 
-  test "roo should get home" do
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    assert_select "title", "Contact | Sample App"
+  end
+
+  test "root should get home" do
     get "/"
     assert_response :success
     assert_select "title", "Sample App"
