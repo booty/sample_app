7026808a0890197f7e550ae35f842210cf8ae13b
booty <johnedmundrose@gmail.com>
2019-01-10 17:50:43 -0500

    User seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  User.create!(name: "testuser#{i}", email: "testuser#{i}@example.com", password: "123456")
+end
