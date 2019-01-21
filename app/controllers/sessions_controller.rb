2350be84038d2ce1065b1964e3e6604f7ef5eada
booty <johnedmundrose@gmail.com>
2019-01-21 18:48:10 -0500

    Users can log out and be forgotten

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in(user)
+      if params[:session][:remember_me] == '1'
+        remember(user)
+      else
+        forget(user)
+      end
       flash[:info] = "Welcome back, you stud"
       redirect_to user
     else
--- test/integration/user_logout_test.rb
+++ test/integration/user_logout_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class UserLogoutTest < ActionDispatch::IntegrationTest
   test "users get logged the fuck out when they want" do
+    @user = users(:michael)
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+
+
     delete logout_path
     follow_redirect!
     assert !is_logged_in?
