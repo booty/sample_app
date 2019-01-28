1d1ec300062677947ad1b40337377755f7f7f0ab
booty <johnedmundrose@gmail.com>
2019-01-28 01:49:20 -0500

    Test login w/ and w/o remembering

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -23,7 +23,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test 'login with valid information like a boss' do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    log_in_as(@user)
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -34,7 +34,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test 'login with valid information followed by logout' do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    log_in_as(@user)
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
@@ -51,5 +51,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
     assert_select 'a[href=?]', user_path(@user), count: 0
+  end
+
+  test 'login_with_remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test 'login_without_remembering' do
+    log_in_as(@user, remember_me: '1')
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
